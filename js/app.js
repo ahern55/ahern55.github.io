8c3cab958a826a6b882457b6853bf4c3cbc08eab
ahern55 <ahern.55@osu.edu>
2021-05-03 20:28:17 -0400

    mobile chopiness

--- js/app.js
+++ js/app.js
@@ -35,10 +35,10 @@ $(function () {
      .addTo(controller);
 
    var animation = new TimelineMax()
-         .fromTo("#socials-wrapper", 1, {opacity: 0}, {opacity: 1, ease: Linear.easeNone})  // fades in
-         .fromTo("section.panel.second", 1, {x: "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
-         .fromTo("#resume", 1, {opacity: 1}, {opacity: 0, ease: Linear.easeNone})  // fades out
-         .fromTo("#timeline", 10, {y: "0%"}, {y: "-100%", ease: Linear.easeNone}) //timeline scroll
+         .fromTo("#socials-wrapper", 10, {opacity: 0}, {opacity: 1, ease: Linear.easeNone})  // fades in
+         .fromTo("section.panel.second", 10, {x: "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
+         .fromTo("#resume", 5, {opacity: 1}, {opacity: 0, ease: Linear.easeNone})  // fades out
+         //.fromTo("#timeline", 10, {y: "0%"}, {y: "-100%", ease: Linear.easeNone}) //timeline scroll
          // create scene to pin and link animation
      new ScrollMagic.Scene({
          triggerElement: "#info-sections",
