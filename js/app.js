afa709268c83e036f0820643352ff3555980e206
ahern55 <ahern.55@osu.edu>
2021-07-14 20:18:17 -0400

    more projects work!

--- js/app.js
+++ js/app.js
@@ -123,4 +123,17 @@ $(function () {
       },
     });
   });
+
+  /* Lightbox - Magnific Popup */
+  $(".popup-with-move-anim").magnificPopup({
+    type: "inline",
+    fixedContentPos: false /* keep it false to avoid html tag shift with margin-right: 17px */,
+    fixedBgPos: true,
+    overflowY: "auto",
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: "my-mfp-slide-bottom",
+  });
 });
